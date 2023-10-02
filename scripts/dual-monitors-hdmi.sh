1781a6ca146413120198f9102671aeaf01f31c1e
david <me@mail.org>
2023-10-02 13:19:30 +0200

    Added xf86 hotkeys for volume and backlight

--- scripts/dual-monitors-hdmi.sh
+++ scripts/dual-monitors-hdmi.sh
@@ -1,3 +1,10 @@
-#activate and set 
 #!/bin/sh
+
+#activate and set monitors using xrandr
 xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DisplayPort-0 --off
+
+#set bspwm workspaces
+bspc monitor eDP -d 1 2 3 4 5 6 7 8 9
+bspc monitor HDMI-A-0 -d  10
+
+# nitrogen  
